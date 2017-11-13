4ee830faf91a4d576660e3e7cb3641e06b6b35f0
robmachado <linux.rlm@gmail.com>
2017-11-13 13:02:41 -0200

    [UPD] 5

--- app/Http/Controllers/AnaliseController.php
+++ app/Http/Controllers/AnaliseController.php
@@ -167,6 +167,8 @@ class AnaliseController extends Controller
     </div>
 </div>';
     
+    public $pgtos = ['0', '15', '30', '45', '60'];
+    
     public function index()
     {
         $dyes = DB::table('dyeings')->select('id','class')->orderBy('class')->pluck('class', 'id');
@@ -183,11 +185,11 @@ class AnaliseController extends Controller
             $tab = $ret['tab'];
             return View::make('tabela',compact(['params','tab']));
         }
+        
         $artigo_id = $request->artigo;
         $tingimento_id = $request->tingimento;
         $destino_id  = $request->destino;
         $pagamento = $request->pagamento;
-        
         
         $artigo = Bom::findOrFail($artigo_id);
         $tingimento = Dyeing::findOrFail($tingimento_id);
@@ -212,11 +214,17 @@ class AnaliseController extends Controller
         
         $custoDireto = 0;
         if ($destino->icms == 0) {
+            $v1 = $raw1->valueicms;
+            $v2 = $raw2->valueicms;
+            $v3 = $raw3->valueicms;
             //venda SP, usar os custos com ICMS
             $custoMP = $artigo->perc1 * $raw1->valueicms
                 + $artigo->perc2 * $raw2->valueicms
                 + $artigo->perc3 * $raw3->valueicms;
         } else {
+            $v1 = $raw1->value;
+            $v2 = $raw2->value;
+            $v3 = $raw3->value;
             //venda !SP, usar os custos sem ICMS
             $custoMP = $artigo->perc1 * $raw1->value
                 + $artigo->perc2 * $raw2->value
@@ -237,40 +245,77 @@ class AnaliseController extends Controller
             + $criterios->ir;
         
         $custoTotal = ($custoDireto+$custoIndireto)/(1-$markup);
-        $juros = (1 + $criterios->financial_rate)**($request->pagamento/30);
-        $custoFinal = (float) $custoTotal * $juros;
-        $j = ($juros-1)*100;
-        $perda = $artigo->losses*100;
-        $icms = $destino->icms*100;
-        $custo = [
-            'ci'=>$custoIndireto,
-            'cm'=> $custoMalha,
-            'cd'=>$custoDireto,
-            'mup'=>$markup,
-            'ct'=>$custoTotal,
-            'cf'=> $custoFinal,
-            'perda'=>$perda,
-            'juros'=>$j,
-            'icms'=>$icms
-        ];
-        $tab[] = [
+        //$custo = [
+        //    'ci'=>$custoIndireto,
+        //    'cm'=> $custoMalha,
+        //    'cd'=>$custoDireto,
+        //    'mup'=>$markup,
+        //    'ct'=>$custoTotal,
+        //    'cf'=> $custoFinal,
+        //    'perda'=>$perda,
+        //    'juros'=>$j,
+        //    'icms'=>$icms
+        //];
+        $custo = [];
+        foreach ($this->pgtos as $pgto) {
+            $juros = (1 + $criterios->financial_rate)**($pgto/30);
+            $custoFinal = (float) $custoTotal * $juros;
+            $custo[$pgto] = round($custoFinal, 2);
+        }
+            
+        $std = new \stdClass();
+        $std->article = $artigo->article;
+        $std->operational_cost = $criterios->operational_cost;
+        $std->financial_cost = $criterios->financial_cost;
+        $std->apportionment = $criterios->apportionment;
+        $std->raw1=$artigo->raw1;
+        $std->raw2=$artigo->raw2;
+        $std->raw3=$artigo->raw3;
+        $std->v1 = $v1;
+        $std->v2 = $v2;
+        $std->v3 = $v3;
+        $std->perc1 = $artigo->perc1;
+        $std->perc2 = $artigo->perc2;
+        $std->perc3 = $artigo->perc3;
+        $std->custoMP = $custoMP;
+        $std->knittings_cod = $artigo->knittings_cod;
+        $std->price = $malharia->price;
+        $std->custoMalha = $custoMalha;
+        $std->class = $tingimento->class;
+        $std->value = $tingimento->value;
+        $std->losses = $artigo->losses;
+        $std->custoDireto = $custoDireto;
+        $std->custoIndireto = $custoIndireto;
+        $std->icms = $destino->icms;
+        $std->ipi = $criterios->ipi;
+        $std->pis = $criterios->pis;
+        $std->cofins = $criterios->cofins;
+        $std->csll = $criterios->csll;
+        $std->ir = $criterios->ir;
+        $std->commission = $criterios->commission;
+        $std->profit = $criterios->profit;
+        $std->markup = $markup;
+        $std->custoTotal = $custoTotal;
+        $explain = $this->explain($std);
+        
+        $tab = [
            'artigo' => $artigo->article,
            'descricao' => $artigo->description,
            'composition' => $artigo->composition,
-           'custo' => $custo
+           'custo' => $custo,
+           'explain' => $explain
         ];
         $params = [
             'destino' => $destino->destination,
-            'tingimento'=>$tingimento->class,
-            'icms'=>$icms
+            'tingimento'=> $tingimento->class,
+            'icms'=> $destino->icms
         ];
         return View::make('retorno',compact(['params','tab']));
-        
     }
     
     public function tabela(Request $request)
     {
-        $pgtos = ['0', '15', '30', '45', '60'];
+        $pgtos = $this->pgtos;
         
         $tingimento_id = $request->tingimento;
         $destino_id  = $request->destino;
@@ -407,5 +452,49 @@ class AnaliseController extends Controller
             'icms'=>$icms
         ];
         return ['params'=>$params, 'tab'=>$tab];
+    }
+    
+    protected function explain(\stdClass $std)
+    {
+        $tmp = str_replace('{{ artigo }}',$std->article,$this->template);
+        $tmp = str_replace('{{ nome }}',$std->article,$tmp);
+        $tmp = str_replace('{{ custooperacioal }}',number_format($std->operational_cost, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ custofinanceiro }}',number_format($std->financial_cost, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ custoindiretototal }}',number_format($std->operational_cost + $std->financial_cost, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ rateio }}',number_format($std->apportionment, 0, ',', '.'),$tmp);
+        $tmp = str_replace('{{ custoindireto }}',number_format(($std->operational_cost + $std->financial_cost)/$std->apportionment, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ referencia1 }}',$std->raw1,$tmp);
+        $tmp = str_replace('{{ referencia2 }}',$std->raw2,$tmp);
+        $tmp = str_replace('{{ referencia3 }}',$std->raw3,$tmp);
+        $tmp = str_replace('{{ valor1 }}',number_format($std->v1, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ valor2 }}',number_format($std->v2, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ valor3 }}',number_format($std->v3, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ p1 }}',number_format($std->perc1 * 100, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ p2 }}',number_format($std->perc2 * 100, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ p3 }}',number_format($std->perc3 * 100, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ preco1 }}',number_format($std->perc1 * $std->v1, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ preco2 }}',number_format($std->perc2 * $std->v2, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ preco3 }}',number_format($std->perc3 * $std->v3, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ precomp }}',number_format($std->custoMP, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ cod }}',$std->knittings_cod,$tmp);
+        $tmp = str_replace('{{ valorkni }}',number_format($std->price, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ mpmal }}',number_format($std->custoMalha, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ tipo }}',$std->class,$tmp);
+        $tmp = str_replace('{{ valortint }}',number_format($std->value, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ losses }}',number_format($std->losses*100, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ custodir }}',number_format($std->custoDireto, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ custoprod }}',number_format($std->custoDireto+$std->custoIndireto, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ icms }}',number_format($std->icms*100, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ ipi }}',number_format($std->ipi*100, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ pis }}',number_format($std->pis*100, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ cofins }}',number_format($std->cofins*100, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ csll }}',number_format($std->csll*100, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ ir }}',number_format($std->ir*100, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ comiss }}',number_format($std->commission*100, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ lucro }}',number_format($std->profit*100, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ markup }}',number_format($std->markup*100, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ complementar }}',number_format((1-$std->markup)*100, 2, ',', '.'),$tmp);
+        $tmp = str_replace('{{ final }}',number_format($std->custoTotal, 2, ',', '.'),$tmp);
+        return $tmp;    
     }
 }
--- resources/views/retorno.blade.php
+++ resources/views/retorno.blade.php
@@ -1,8 +1,35 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+@extends('layouts.app')
+@section('content')
+<h2 class="page-header">Análise de Preço de Venda</h2>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <h4> {{ $tab['artigo'] }} - {{ $tab['descricao'] }} ({{ $tab['composition'] }})</h4>
+        <p>Parâmetros <i class="fa fa-pencil-square-o" aria-hidden="true" style="color:#3978A7" data-toggle="modal" data-target="#{{ $tab['artigo'] }}"></i></p>
+        <p>{{ $params['destino'] }} <strong> {{ 'ICMS: ' . number_format($params['icms']*100,0,'','') }}% </strong> Tingimento: {{ $params['tingimento'] }}</p>
+        <br/>
+        <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>À vista</th>
+                    <th>15 dd</th>
+                    <th>30 dd</th>
+                    <th>45 dd</th>
+                    <th>60 dd</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td width="170px">R$ {{ $tab['custo'][0] }}/kg</td>
+                    <td width="170px">R$ {{ $tab['custo'][15] }}/kg</td>
+                    <td width="170px">R$ {{ $tab['custo'][30] }}/kg</td>
+                    <td width="170px">R$ {{ $tab['custo'][45] }}/kg</td>
+                    <td width="170px">R$ {{ $tab['custo'][60] }}/kg</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>    
+    </div>
+</div>
+{!! html_entity_decode($tab['explain']) !!}<br>
+@endsection
