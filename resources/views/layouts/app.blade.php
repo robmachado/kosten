a552c6bffd538b35482608655d9dd25e45a03383
robmachado <linux.rlm@gmail.com>
2017-11-24 11:49:31 -0200

    [UPD] c9

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,9 +40,8 @@
                     <ul class="nav navbar-nav navbar-right">
                         @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
                         @else
-                            
+                            <li><a href="{{ route('register') }}">Register</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
                                     {{ Auth::user()->name }} <span class="caret"></span>
--- routes/web.php
+++ routes/web.php
@@ -14,19 +14,18 @@
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-
-Route::get('/', 'AnaliseController@index')->name('analise.index');
-
 Auth::routes();
-
+Route::get('/', 'AnaliseController@index')->name('analise.index');
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/analise', 'AnaliseController@index')->name('analise.index');
 Route::post('/analise', 'AnaliseController@calcular')->name('analise.calcular');
 
-Route::resource('/criterias', 'CriteriasController');
-Route::resource('/dyeings', 'DyeingsController');
-Route::resource('/rawmaterials', 'RawmaterialsController');
-Route::resource('/boms', 'BomsController');
-Route::resource('/destinations', 'DestinationsController');
-Route::resource('/knittings', 'KnittingsController');
-Route::resource('/packagings', 'PackagingsController');
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('/criterias', 'CriteriasController');
+    Route::resource('/dyeings', 'DyeingsController');
+    Route::resource('/rawmaterials', 'RawmaterialsController');
+    Route::resource('/boms', 'BomsController');
+    Route::resource('/destinations', 'DestinationsController');
+    Route::resource('/knittings', 'KnittingsController');
+    Route::resource('/packagings', 'PackagingsController');
+});
